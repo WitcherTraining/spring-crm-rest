b423c53146aa5bb0c9a88969dcfaf19eddd87cce
WitcherTraining <witcher236@gmail.com>
2020-12-20 10:32:54 +0600

    Added JWT security

--- src/main/java/com/luv2code/springdemo/config/SecurityConfig.java
+++ src/main/java/com/luv2code/springdemo/config/SecurityConfig.java
@@ -8,19 +8,19 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 
-@Configuration
-@EnableWebSecurity
+//@Configuration
+//@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-        User.UserBuilder users = User.withDefaultPasswordEncoder();
-
-        auth.inMemoryAuthentication()
-                .withUser(users.username("witcher").password("dummy").roles("ADMIN"));
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//
+//        User.UserBuilder users = User.withDefaultPasswordEncoder();
+//
+//        auth.inMemoryAuthentication()
+//                .withUser(users.username("witcher").password("dummy").roles("ADMIN"));
 //                .withUser(users.username("Denis").password("test123").roles("AUTHOR"))
 //                .withUser(users.username("Petr").password("test123").roles("AUTHOR"));
-    }
+//    }
 
 //    @Override
 //    protected void configure(HttpSecurity http) throws Exception {
@@ -34,16 +34,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //                .permitAll();
 //    }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .authorizeRequests()
-                .antMatchers(HttpMethod.OPTIONS,"/**")
-                .permitAll()
-                .anyRequest().authenticated()
-                .and()
-                //.formLogin().and()
-                .httpBasic();
-    }
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .csrf().disable()
+//                .authorizeRequests()
+//                .antMatchers(HttpMethod.OPTIONS,"/**")
+//                .permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//                //.formLogin().and()
+//                .httpBasic();
+//    }
 }
---
+++ src/main/java/com/luv2code/springdemo/jwt/JwtInMemoryUserDetailsService.java
@@ -0,0 +1,38 @@
+package com.luv2code.springdemo.jwt;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class JwtInMemoryUserDetailsService implements UserDetailsService {
+
+    static List<JwtUserDetails> inMemoryUserList = new ArrayList<>();
+
+    static {
+        // pass - dummy
+        inMemoryUserList.add(new JwtUserDetails(1L, "admin",
+                "$2a$10$3zHzb.Npv1hfZbLEU5qsdOju/tk2je6W6PnNnY.c1ujWPcZh4PL6e", "ROLE_USER_1"));
+        // pass - password123
+        inMemoryUserList.add(new JwtUserDetails(2L, "witcher",
+                "$2a$10$G307J7DKFVsWLjCJnQnRAuYDAPqAXShOEroXXYmGnFLf/rBf8boG6", "ROLE_USER_2"));
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<JwtUserDetails> findFirst = inMemoryUserList.stream()
+                .filter(user -> user.getUsername().equals(username)).findFirst();
+
+        if (!findFirst.isPresent()) {
+            throw new UsernameNotFoundException(String.format("USER_NOT_FOUND '%s'.", username));
+        }
+
+        return findFirst.get();
+    }
+
+}
